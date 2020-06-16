19930f810b5f6caf1af9ad023c26d267ed629dbf
zaheet <47720053+zeedev786@users.noreply.github.com>
2020-06-16 18:39:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+var app =  angular.module("HangmanApp",[]);
+app.controller("GameController",['$scope','$timeout',function($scope,$timeout){
+    var words=["rat","zaeeh","sim","mat"];
+    $scope.incorrectLettersChosen=[];
+    $scope.correctLettersChosem=[];
+    $scope.guesses = 6;
+    $scope.displayWord = '';
+    $scope.input = {
+        letter : ''
+    }
+
+    var selectRandomWord =  function(){
+        var index = Math.round(Math.random()*words.length);
+        return words[index];
+    }
+
+    var newGame = function(){
+
+        $scope.incorrectLettersChosen = [];
+        $scope.correctLettersChosen=[];
+        $scope.guesses = 6;
+        $scope.displayWord = '';
+
+        selectedWord = selectRandomWord();
+        var tempDisplayWord = '';
+    
+        console.log(selectedWord);
+        for (var i = 0; i < selectedWord.length; i++) {
+            tempDisplayWord += '*';
+        }
+        displayWord = tempDisplayWord;
+        $scope.displayWord =  displayWord
+        console.log(displayWord);
+    }
+
+    $scope.letterChosen = function(){
+        for (var i = 0; i <$scope.correctLettersChosen.length; i++) {
+            if ($scope.correctLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase()) {
+                $scope.input.letter="";
+                return;
+            }
+        }
+        
+        for (var i = 0; i < $scope.incorrectLettersChosen.length; i++) {
+            if ($scope.incorrectLettersChosen[i].toLowerCase()== $scope.input.letter.toLowerCase()) {
+                $scope.input.letter="";
+                return;
+            }
+            
+        }
+
+        var correct = false;
+        for (var i = 0; i < selectedWord.length; i++) {
+            if (selectedWord[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+                $scope.displayWord = $scope.displayWord.slice(0,i)+ $scope.input.letter.toLowerCase()+$scope.displayWord.slice(i+1);
+                correct = true;
+                
+            }
+        }
+        if(correct){
+            $scope.correctLettersChosen.push($scope.input.letter.toLowerCase());
+        }else{
+            $scope.guesses--;
+            $scope.incorrectLettersChosen.push($scope.input.letter.toLowerCase());
+        }
+        $scope.input.letter = "";
+        if($scope.guesses==0){
+           
+            $timeout(function(){
+              newGame();  
+            },500);
+        }
+        if($scope.displayWord.indexOf("*")==-1){
+            alert("you won yeeeeh! <3 ");
+        }
+    }
+
+    newGame();
+}]);
